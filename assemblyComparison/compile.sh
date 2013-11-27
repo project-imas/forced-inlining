4017a317b5c79bb5b82c21c3373742dd2c8a5e75
Gavin Black <gblack@mitre.org>
2013-11-27 07:17:21 -0500

    Updated for clang

--- assemblyComparison/compile.sh
+++ assemblyComparison/compile.sh
@@ -2,7 +2,9 @@
 
 if [ "$#" -eq "1" ]
 then
-  gcc -finline-limit=5000 -DALWAYS_INLINE=1 -O1 -S -o TestAlways.asm Test.c
+  gcc -finline-limit=5000 -DALWAYS_INLINE=1 -O1 -S -o GccAlways.asm Test.c
+  clang -DALWAYS_INLINE=1 -O1 -S -o ClangAlways.asm Test.c
 else
-  gcc -O1 -S -o TestNormal.asm Test.c
+  gcc -O1 -S -o GccNormal.asm Test.c
+  clang -O1 -S -o ClangNormal.asm Test.c
 fi
